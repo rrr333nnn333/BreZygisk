af2742fb380c723093e64615a19429002f2eb3f1
Rene <rrr333nnn333@gmail.com>
2026-04-06 00:50:44 -0400

    rename: ReZygisk to BreZygisk partially

--- module/src/uninstall.sh
+++ module/src/uninstall.sh
@@ -2,11 +2,11 @@
 
 set -e
 
-export TMP_PATH=/data/adb/rezygisk
+export TMP_PATH=/data/adb/brezygisk
 rm -rf "$TMP_PATH"
 
-rm -f /data/adb/post-fs-data.d/rezygisk.sh
-rm -f /data/adb/post-mount.d/rezygisk.sh
+rm -f /data/adb/post-fs-data.d/brezygisk.sh
+rm -f /data/adb/post-mount.d/brezygisk.sh
 
 # INFO: Only removes if dir is empty
 rmdir /data/adb/post-fs-data.d
